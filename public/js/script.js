7f9bc9b1dbb2d1178915d2b35072fe3ed5ba8b60
Nathaniel Poulson <nat.poulson@outlook.com.au>
2022-06-19 22:06:20 +1000

    first commit

---
+++ public/js/script.js
@@ -0,0 +1,10 @@
+// Drum Arrays
+let kicks = new Array(16).fill(false);
+let snares = new Array(16).fill(false);
+let hiHats = new Array(16).fill(false);
+let rideCymbals = new Array(16).fill(false);
+
+function toggleDrum(drum, index) {
+    // Check the inputs and ensure they're valid
+    // Instrument check
+}
